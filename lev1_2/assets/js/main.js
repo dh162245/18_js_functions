91d9b6a00bd4c6561d4a5f559bc07a634b3245f0
dh162245 <daniel.huck@hotmail.co.uk>
2022-01-18 14:49:50 +0100

    1

---
+++ lev1_2/assets/js/main.js
@@ -0,0 +1,3 @@
+let gallery = document.getElementById('gallery')
+
+gallery.innerHTML = '<figure><img src="assets/img/11.jpg" alt=""><figcaption> Fig 1 </figcaption></figure> <figure><img src="assets/img/11.jpg" alt=""><figcaption> Fig 2 </figcaption></figure> <figure><img src="assets/img/11.jpg" alt=""><figcaption> Fig 3 </figcaption></figure>'
